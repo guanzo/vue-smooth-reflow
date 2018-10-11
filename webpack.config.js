2b1f87baf9c863aa84a1ee9b0c21dd134ccd43c8
David Eglin <david.eglin@mccann.com>
2018-10-11 15:51:13 +0100

    update config with proper env config

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ const config = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: [['@babel/preset-env'. {
+                        presets: [['@babel/preset-env', {
                             targets: {
                                 browsers: ['last 2 versions', 'safari >= 9', '> 1%', 'IE 11']       
                             }
